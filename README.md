50ca6a65f1f0937addc712b095d7e092f4f5aa0c
Zachary Godfrey <godfrey.zach@gmail.com>
2019-09-15 10:00:30 -0400

    overhaul

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ A redesign of my personal website.
 
 - Run `npm i` to install dependencies
 - Run `npm start` to start the local server
-- Site is available at `http://localhost:5000`
+- Site is available at `http://localhost:8080`
--- src/build.js
+++ src/build.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const fs = require('fs-extra');
 const scss = require('node-sass');
-const mustache = require('mustache');
 
 // Helper Functions
 
@@ -19,50 +18,33 @@ const memoize = (action) => {
 
 const resolve = (filePath) => path.resolve(path.join(__dirname, filePath));
 const readFile = (filePath) => fs.readFileSync(resolve(filePath), 'utf-8');
-const readJson = (filePath) => fs.readJsonSync(resolve(filePath), 'utf-8');
 const writeFile = (filePath, content) => fs.outputFileSync(resolve(filePath), content, 'utf-8');
 
 const src = memoize((filePath) => readFile(`./${filePath}`));
 const dist = (filePath, content) => writeFile(`../dist/${filePath}`, content);
 const cleanDist = () => fs.emptyDirSync(resolve('../dist'));
+const copyStatic = () => fs.copySync(resolve('./static'), resolve('../dist'));
 
 const compileSCSS = (fileContent) => scss.renderSync({
   data: fileContent,
   outputStyle: 'compressed'
 }).css.toString();
 
-// Build Pages
+// Build Page
+
+const content = src('index.html');
+const styles = compileSCSS([
+  "/style/variables.scss",
+  "/style/reset.scss",
+  "/style/fonts.scss",
+  "/style/utilities.scss",
+  "/style/components.scss",
+].map(file => src(file)).join('\n'));
+
+const page = content.replace('/* Styles */', styles);
+
+// Output Files
 
 cleanDist();
-
-const config = readJson('config.json');
-
-config.pages.forEach(page => {
-  const globalStyles = config.shared.stylesheets.map(file => src(file));
-  const pageStyles = (page.stylesheets || []).map(file => src(file));
-  const stylesheet = compileSCSS(globalStyles.concat(pageStyles).join('\n'));
-
-  const globalScripts = config.shared.scripts.map(file => src(file));
-  const pageScripts = (page.scripts || []).map(file => src(file));
-  const script = globalScripts.concat(pageScripts).join('\n');
-
-  const destination = `${page.url}/index.html`;
-  const content = src(page.content);
-  const layout = src(page.layout)
-  .replace('/* Stylesheet */', stylesheet)
-  .replace('/* Script */', script);
-
-  const globalPartials = Object.keys(config.shared.partials).reduce((obj, key) => {
-    return Object.assign(obj, { [key]: src(config.shared.partials[key]) });
-  }, {});
-
-  const pagePartials = Object.keys(page.partials || {}).reduce((obj, key) => {
-    return Object.assign(obj, { [key]: src(page.partials[key]) });
-  }, {});
-
-  const partials = Object.assign({}, globalPartials, pagePartials, {
-    page: content
-  });
-
-  dist(destination, mustache.render(layout, page.data, partials));
-});
+copyStatic();
+dist('index.html', page);
