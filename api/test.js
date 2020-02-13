fc6903b2475dfefe351b85fd883671716c45b1b6
Zachary Godfrey <zachary.godfrey@bamtech.net>
2020-02-13 17:16:54 -0500

    API function

--- api/test.js
+++ api/test.js
@@ -1,12 +1,17 @@
+const test = (event, context) => {
+    return {
+        event: Object.keys(event),
+        context: Object.keys(context),
+        data: {
+            works: true
+        },
+        message: `works!`
+    }
+};
+
 exports.handler = (event, context, callback) => {
     callback({
         statusCode: 200,
-        body: JSON.stringify({
-            event,
-            context,
-            data: {
-                works: true
-            }
-        }, null, 2)
+        body: JSON.stringify(test(event, context))
     });
 };
