9594b12bf8f181fe625bbe47f779286b5636488f
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2024-07-17 09:58:52 -0700

    Prevent warning message

--- build.js
+++ build.js
@@ -22,7 +22,12 @@ const writeFile = (filePath, content, encoding) => fs.outputFileSync(resolve(fil
 const emptyFolder = (folderPath) => fs.emptyDirSync(resolve(folderPath));
 const copyFolder = (srcPath, destPath) => fs.copySync(resolve(srcPath), resolve(destPath));
 const compileMD = (fileContent) => marked.parse(fileContent, { gfm: true });
-const minifyCSS = async (fileContent) => await postcss([cssnano]).process(fileContent).then(({ css }) => css);
+
+const minifyCSS = async (fileContent) => {
+  return await postcss([cssnano]).process(fileContent, {
+    from: undefined
+  }).then(({ css }) => css);
+};
 
 const parseMetadata = (fileContent) => {
   const { attributes: meta, body: content } = frontMatter(fileContent);
