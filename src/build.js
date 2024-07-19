a67eea60711ed6ac7d32b1f3c8d8311500d88a80
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2024-07-19 08:25:56 -0700

    Refactor build script

--- src/build.js
+++ src/build.js
@@ -1,14 +1,9 @@
 import { readFile, listFiles, emptyFolder, copyFolder, writeFile } from '../lib/file.js';
 import { renderMD, minifyCSS, parseMetadata, renderMustache } from '../lib/transform.js';
 import { renderEmoji } from '../lib/emoji.js';
+import { byAscending } from '../lib/misc.js';
 
 const START = Date.now();
-
-const byAscending = (fn) => (left, right) => {
-  const l = fn(left), r = fn(right);
-
-  return l < r ? -1 : l > r ? 1 : 0;
-};
 
 const shell = readFile('src/assets/shell.html');
 const favicon = readFile('src/assets/terminal.png', 'base64');
@@ -16,6 +11,7 @@ const fontFancy = readFile('src/assets/satisfy.ttf', 'base64');
 const fontMono = readFile('src/assets/roboto-mono.ttf', 'base64');
 const style = await minifyCSS(readFile('src/assets/style.css'));
 const hero = readFile('src/static/animated.png', 'base64');
+const partials = { favicon, fontFancy, fontMono, style, hero };
 
 const pages = listFiles('src/pages/**/*.md').map(filePath => {
   const uri = filePath.split('pages/')[1].replace('.md', '');
@@ -25,11 +21,7 @@ const pages = listFiles('src/pages/**/*.md').map(filePath => {
   return { uri, meta, content };
 });
 
-const posts = pages
-  .filter(x => x.meta.type == 'post')
-  .sort(byAscending(x => x.meta.order));
-
-const partials = { favicon, fontFancy, fontMono, style, hero };
+const posts = pages.filter(x => x.meta.type == 'post').sort(byAscending(x => x.meta.order));
 
 emptyFolder('dist');
 copyFolder('src/static', 'dist');
