564d0736d4c6f82f5ced206d1cf8433ae808ccb9
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2024-08-22 12:31:49 -0400

    WIP

--- src/build.js
+++ src/build.js
@@ -6,18 +6,13 @@ import { byAscending } from '../lib/misc.js';
 const START = Date.now();
 
 const shell = readFile('src/assets/shell.html');
-const partials = listFiles('src/partials/*.html').reduce((obj, filePath) => {
-  const name = filePath.split('partials/')[1].replace('.html', '');
 
-  obj[name] = `\n${readFile(`./${filePath}`)}\n`;
-
-  return obj;
-}, {
+const partials = {
   favicon: readFile('src/assets/terminal.png', 'base64'),
   fontFancy: readFile('src/assets/satisfy.ttf', 'base64'),
   style: await minifyCSS(readFile('src/assets/style.css')),
   hero: readFile('src/static/animated.png', 'base64')
-});
+};
 
 const pages = listFiles('src/pages/**/*.md').map(filePath => {
   const uri = filePath.split('pages/')[1].replace('.md', '');
@@ -27,7 +22,9 @@ const pages = listFiles('src/pages/**/*.md').map(filePath => {
   return { uri, meta, content };
 });
 
-const posts = pages.filter(x => x.meta.type == 'post').sort(byAscending(x => x.meta.order));
+const posts = pages
+  .filter(x => x.meta.type == 'post')
+  .sort(byAscending(x => x.meta.order));
 
 emptyFolder('dist');
 copyFolder('src/static', 'dist');
